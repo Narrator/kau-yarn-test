607368203062d01bc01d0cf8c21d1813310d6b3a
Kaushik Gnanaskandan <kaushik.subman@gmail.com>
2021-11-30 15:06:28 -0800

    Add Hello World Function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "HelloWorld": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
